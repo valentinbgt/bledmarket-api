570fa4dd1ea1d013fd1e60e4a49d5f86aea0585c
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-06-19 22:12:04 +0200

    Update conf.inc.php

--- Conf/conf.inc.php
+++ Conf/conf.inc.php
@@ -1,9 +1,7 @@
 <?php
     define('DB_HOST', 'localhost');
 
-    define('DB_FILES_NAME', 'files');
-    define('DB_USERS_NAME', 'users');
-    define('DB_PICUSERS_NAME', 'pictusers');
+    define('DB_NAME', 'bledmarket');
 
     define('DB_USER', 'bledmarket-api');
     define('DB_PASSWORD', 'wMjEqFpRvUkF3i58UrC67aa6C43L3A683fvz6Bvixm7B9sUjL8Q5H35W9E5RxkU7');
