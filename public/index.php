598f1859402b18ced2a87ed4dc59bc4722d9543c
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-06-20 12:53:28 +0200

    Correction de l'arboressence (errorHandler & Includes)

--- public/index.php
+++ public/index.php
@@ -6,12 +6,12 @@
     require_once(PROJECT_ROOT . 'Class/BledMarket/Api.php');
     $api = new Api;
 
-    require_once(PROJECT_ROOT . 'app/errors/errorHandler.php');
+    require_once(PROJECT_ROOT . 'Includes/errorHandler.inc.php');
 
     if(!file_exists(PROJECT_ROOT . 'Conf/conf.inc.php')) $api->error(3, "Le fichier de configuration de l'application est introuvable. Utilisez /Conf/conf.inc.sample.php pour en créer un.");
     require_once(PROJECT_ROOT . 'Conf/conf.inc.php');
 
     require_once(PROJECT_ROOT . 'app/rooter.php');
-
+    
 
     $api->reply();
