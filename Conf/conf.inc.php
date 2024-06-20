a5bc89c3bac85d21de938d5f32fbe32fe3ce7f11
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-06-20 11:32:00 +0200

    Essaie de faire fonctionner le .gitignore

--- Conf/conf.inc.php
+++ Conf/conf.inc.php
@@ -4,4 +4,4 @@
     define('DB_NAME', 'bledmarket');
 
     define('DB_USER', 'bledmarket-api');
-    define('DB_PASSWORD', 'wMjEqFpRvUkF3i58UrC67aa6C43L3A683fvz6Bvixm7B9sUjL8Q5H35W9E5RxkU7');
+    define('DB_PASSWORD', 'KEY');
