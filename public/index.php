4579e24d585c97f6fe1b74110d663498c2e0df0e
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-08-06 18:31:09 +0200

    Added header("Access-Control-Allow-Origin: *");

--- public/index.php
+++ public/index.php
@@ -1,26 +1,28 @@
 <?php
-    session_start();
+session_start();
 
-    require_once('../Includes/constants.inc.php');
+header("Access-Control-Allow-Origin: *");
 
-    require_once(PROJECT_ROOT . 'Class/BledMarket/Api.php');
-    $api = new Api;
+require_once('../Includes/constants.inc.php');
 
-    require_once(PROJECT_ROOT . 'Includes/errorHandler.inc.php');
+require_once(PROJECT_ROOT . 'Class/BledMarket/Api.php');
+$api = new Api;
 
-    if(!file_exists(PROJECT_ROOT . 'Conf/conf.inc.php')) $api->error(3, "Le fichier de configuration de l'application est introuvable. Utilisez /Conf/conf.inc.sample.php pour en créer un.");
-    require_once(PROJECT_ROOT . 'Conf/conf.inc.php');
+require_once(PROJECT_ROOT . 'Includes/errorHandler.inc.php');
 
-    require_once(PROJECT_ROOT . 'Class/BledMarket/Database.php');
-    $db = new Database();
+if (!file_exists(PROJECT_ROOT . 'Conf/conf.inc.php')) $api->error(3, "Le fichier de configuration de l'application est introuvable. Utilisez /Conf/conf.inc.sample.php pour en créer un.");
+require_once(PROJECT_ROOT . 'Conf/conf.inc.php');
 
-    require_once(PROJECT_ROOT . 'Class/BledMarket/User.php');
-    $user = new User();
+require_once(PROJECT_ROOT . 'Class/BledMarket/Database.php');
+$db = new Database();
 
-    require_once(PROJECT_ROOT . 'Class/BledMarket/File.php');
-    require_once(PROJECT_ROOT . 'Class/BledMarket/Functions.php');
+require_once(PROJECT_ROOT . 'Class/BledMarket/User.php');
+$user = new User();
 
-    require_once(PROJECT_ROOT . 'app/rooter.php');
+require_once(PROJECT_ROOT . 'Class/BledMarket/File.php');
+require_once(PROJECT_ROOT . 'Class/BledMarket/Functions.php');
+
+require_once(PROJECT_ROOT . 'app/rooter.php');
 
 
-    $api->reply();
+$api->reply();
